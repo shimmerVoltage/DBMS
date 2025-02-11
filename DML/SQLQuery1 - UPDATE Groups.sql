4f165e974765351a4c1b42a5c3140bfe726c8e3e
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-02-11 22:43:10 +0300

    Schedule in progress

--- DML/SQLQuery1 - UPDATE Groups.sql
+++ DML/SQLQuery1 - UPDATE Groups.sql
@@ -1,9 +1,9 @@
 USE PV_319_DDL;
 GO
 
-UPDATE Groups
-SET direction=2
-WHERE group_id=4;
+UPDATE Groups SET direction=2 WHERE group_id=4;
+UPDATE Groups SET direction=3 WHERE group_id=5 OR group_id=6
+UPDATE Groups SET direction=4 WHERE group_id=8;
 
 SELECT * FROM Groups;
 		
---
+++ DML/SQLQuery2 - COUNT Teachers and Disciplines.sql
@@ -0,0 +1,20 @@
+USE PV_319_Import;
+GO
+
+SELECT
+			discipline_name		AS N'Дисциплина',
+			COUNT(teacher_id)	AS N'Количество преподавателей'
+FROM		Teachers, Disciplines, TeachersDisciplinesRelation
+WHERE		teacher		=	teacher_id
+AND			discipline	=	discipline_id
+GROUP BY	discipline_name
+;
+
+SELECT
+			discipline_name			AS N'Дисциплина',
+			FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name)	AS N'Преподаватель'
+FROM		Teachers, Disciplines, TeachersDisciplinesRelation
+WHERE		teacher		=	teacher_id
+AND			discipline	=	discipline_id
+ORDER BY	discipline_name
+;
